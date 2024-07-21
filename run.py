4c21cedb3cb4d088d81e8684552fcfcdcc7ed425
AregSPX <54rg5y4n@gmail.com>
2024-07-21 15:22:29 +0400

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from flaskblog import create_app    # when importing from package (folder), it looks at __init__.py
-
-app = create_app()                  # we imported the app creator function and initialized an app with our default configs
-
-if __name__ == '__main__':   # this file's only purpose is to run the app
-    app.run(debug=False)
